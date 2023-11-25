fc4665715310176bff3c7a8864ef88ff786014b9
Josué Henrique <121066937+Mauboru@users.noreply.github.com>
2023-11-25 14:09:15 -0300

    testando classe pessoa com gerenciador

---
+++ gerenciador_pessoas.py
@@ -0,0 +1,35 @@
+from pessoa import Pessoa
+
+class GerenciadorPessoas:
+    def __init__(self):
+        self.pessoas = []
+
+    def adicionar_pessoa(self, pessoa):
+        self.pessoas.append(pessoa)
+
+    def listar_pessoas(self):
+        for pessoa in self.pessoas:
+            print(pessoa)
+
+    def buscar_pessoa(self, nome):
+        for pessoa in self.pessoas:
+            if pessoa.nome == nome:
+                return pessoa
+        return None
+
+    def atualizar_pessoa(self, nome, nova_idade, nova_cidade):
+        pessoa = self.buscar_pessoa(nome)
+        if pessoa:
+            pessoa.idade = nova_idade
+            pessoa.cidade = nova_cidade
+            print(f"{nome} atualizado com sucesso.")
+        else:
+            print(f"{nome} não encontrado.")
+
+    def excluir_pessoa(self, nome):
+        pessoa = self.buscar_pessoa(nome)
+        if pessoa:
+            self.pessoas.remove(pessoa)
+            print(f"{nome} removido com sucesso.")
+        else:
+            print(f"{nome} não encontrado.")
---
+++ main.py
@@ -0,0 +1,13 @@
+from pessoa import Pessoa
+from gerenciador_pessoas import GerenciadorPessoas
+
+gerenciador = GerenciadorPessoas()
+
+pessoa1 = Pessoa("Josue", "josue@gmail.com", 84927049, 22)
+pessoa2 = Pessoa("Vinicius", "vinicius@gmail.com", 12345678, 22)
+
+gerenciador.adicionar_pessoa(pessoa1)
+gerenciador.adicionar_pessoa(pessoa2)
+
+print("Lista de Pessoas:")
+gerenciador.listar_pessoas()
