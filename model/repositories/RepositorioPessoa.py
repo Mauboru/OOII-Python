95561d4f75db8b4b99cff4c3aabc358180b7e738
Josue Henrique <josue21henrique@gmail.com>
2023-12-05 02:54:08 -0300

    corrigindo alguns pontos

--- model/repositories/RepositorioPessoa.py
+++ model/repositories/RepositorioPessoa.py
@@ -5,8 +5,8 @@ class RepositorioPessoa:
         self.pessoaDAO = pessoaDAO
 
     def adicionar_pessoa(self, nome, email, telefone, idade):
-        if nome is None or email is None or telefone is None or idade is None:
-            raise ValueError("Nenhuma das variáveis pode ser nula")
+        if nome == "" or email == "" or telefone == "" or idade is None:
+            return ValueError("Nenhuma das variáveis pode ser nula")
 
         nova_pessoa = Pessoa(nome, email, telefone, idade)
         return self.pessoaDAO.criar(nova_pessoa)
@@ -18,8 +18,8 @@ class RepositorioPessoa:
         return self.pessoaDAO.buscarPorId(id)
 
     def atualizar_pessoa(self, id, novo_nome, novo_email, novo_telefone, novo_idade):
-        if novo_nome is None or novo_email is None or novo_telefone is None or novo_idade is None:
-            raise ValueError("Nenhuma das variáveis pode ser nula")
+        if novo_nome == "" or novo_email == "" or novo_telefone == "" or novo_idade is None:
+            return ValueError("Nenhuma das variáveis pode ser nula")
 
         pessoa_atualizada = Pessoa(novo_nome, novo_email, novo_telefone, novo_idade, id)
         return self.pessoaDAO.atualizar(pessoa_atualizada)
