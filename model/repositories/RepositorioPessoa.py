56734ef105b75b010e16f791af031da259e0bb91
JeanLucasMM <Jeanlucasmachado.m@hotmail.com>
2023-12-05 00:47:41 -0300

    Criada Validações (Adicionar/AtualizarPessoa)

--- model/repositories/RepositorioPessoa.py
+++ model/repositories/RepositorioPessoa.py
@@ -5,6 +5,9 @@ class RepositorioPessoa:
         self.pessoaDAO = pessoaDAO
 
     def adicionar_pessoa(self, nome, email, telefone, idade):
+        if nome is None or email is None or telefone is None or idade is None:
+            raise ValueError("Nenhuma das variáveis pode ser nula")
+
         nova_pessoa = Pessoa(nome, email, telefone, idade)
         return self.pessoaDAO.criar(nova_pessoa)
 
@@ -15,6 +18,9 @@ class RepositorioPessoa:
         return self.pessoaDAO.buscarPorId(id)
 
     def atualizar_pessoa(self, id, novo_nome, novo_email, novo_telefone, novo_idade):
+        if novo_nome is None or novo_email is None or novo_telefone is None or novo_idade is None:
+            raise ValueError("Nenhuma das variáveis pode ser nula")
+
         pessoa_atualizada = Pessoa(novo_nome, novo_email, novo_telefone, novo_idade, id)
         return self.pessoaDAO.atualizar(pessoa_atualizada)
 
